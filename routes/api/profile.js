898814b5aeab2bfc342ab1e684743c2db6c7c042
Samid77 <dimaseptyanto@windowslive.com>
2018-09-18 19:44:19 +0700

    Add education and experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 const passport = require('passport');
 
 /** Load validation */
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/Education');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -217,6 +220,72 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         })
 
 })
+
+/**
+ * @route POST api/profile/experience
+ * @desc Add experience to profile
+ * @access Private
+ */
+
+ router.post('/experience', passport.authenticate('jwt', {session: false}), (req, res) => {
+    
+    const {errors, isValid} = validateExperienceInput(req.body);
+    if(!isValid){
+        /** Return any errors */
+        res.status(400).json(errors);
+    }
+
+     Profile
+        .findOne({user: req.user.id})
+        .then((profile) => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description,
+            }
+            /** Add to experience array */
+            profile.experience.unshift(newExp);
+            profile.save().then((profile) => {res.json(profile)});
+        })
+ })
+
+ /**
+ * @route POST api/profile/education
+ * @desc Add education to profile
+ * @access Private
+ */
+
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+      const { errors, isValid } = validateEducationInput(req.body);
+  
+      /** Check validation */
+      if (!isValid) {
+        /** Return any errors with 400 status **/
+        return res.status(400).json(errors);
+      }
+  
+      Profile.findOne({ user: req.user.id }).then(profile => {
+        const newEdu = {
+          school: req.body.school,
+          degree: req.body.degree,
+          fieldofstudy: req.body.fieldofstudy,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
+  
+        profile.education.unshift(newEdu);
+        profile.save().then(profile => res.json(profile));
+        
+      });
+    }
+  );
+
 
 
 module.exports = router;
